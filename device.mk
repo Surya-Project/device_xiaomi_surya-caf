681be27b41605c121f38cacd249de0af9afe54e6
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-11-30 15:33:46 -0400

    surya: Update drm service.clearkey to 1.4
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Change-Id: I0c70023a656e6b19fb2bbf8c57de6b0cb05f6e05
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -78,7 +78,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3 \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.3-service.widevine \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 # Filesystems table
 PRODUCT_COPY_FILES += \
