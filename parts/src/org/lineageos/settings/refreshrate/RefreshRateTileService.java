1ffc8fe98909a3e0ba62dc2eef8b2099a9f51cc1
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-05-02 23:00:26 +0000

    surya: parts: Move RefreshRateTile to refreshrate folder
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- parts/src/org/lineageos/settings/refreshrate/RefreshRateTileService.java
+++ parts/src/org/lineageos/settings/refreshrate/RefreshRateTileService.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.lineageos.settings;
+package org.lineageos.settings.refreshrate;
 
 import android.content.Context;
 import android.provider.Settings;
